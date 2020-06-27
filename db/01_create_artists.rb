ea0e24fc43b463fd1fdb1aefd56ad82948d140c3
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 00:22:13 +0000

    Automatically backed up by Learn

---
+++ db/01_create_artists.rb
@@ -0,0 +1,33 @@
+require '../config/environment.rb'
+
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
+
+  def down
+  end
+end
+
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS artists (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    genre TEXT,
+    age INTEGER,
+    hometown TEXT
+    )
+  SQL
+
+  ActiveRecord::Base.connection.execute(sql)
+
+end
