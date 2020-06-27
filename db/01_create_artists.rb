7a29e94eaafddd5baebcd66310f4e5999ba39ba9
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 00:23:08 +0000

    Automatically backed up by Learn

--- db/01_create_artists.rb
+++ db/01_create_artists.rb
@@ -18,16 +18,4 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
   end
 
-  sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS artists (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    genre TEXT,
-    age INTEGER,
-    hometown TEXT
-    )
-  SQL
-
-  ActiveRecord::Base.connection.execute(sql)
-
 end
